719fb65be9deda38c5d281548b1d4fcd3e127ba6
Emily Ingraham <emilyvingraham@gmail.com>
2023-12-28 09:43:47 -0700

    Configured api weather data response

--- src/index.js
+++ src/index.js
@@ -1,16 +1,65 @@
 function update(response) {
   let temperatureElement = document.querySelector('#current-temperature');
-  let temperature = Math.round(response.data.temperature.current);
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector('#weather-app-city');
+  let descriptionElement = document.querySelector('#description');
+  let humidityElement = document.querySelector('#api-humidity');
+  let windspeedElement = document.querySelector('#api-windspeed');
+  let feelsLikeElement = document.querySelector('#api-feels-like');
+  let feelsLike = Math.round(response.data.temperature.feels_like);
+  let locationCityElement = document.querySelector('#location-city');
+  let locationCity = response.data.city;
+  let locationCountryElement = document.querySelector('#location-country');
+  let locationCountry = response.data.country;
+  let longitudeElement = document.querySelector('#longitude');
+  let latitudeElement = document.querySelector('#latitude');
+  let timeElement = document.querySelector('#time');
+  let date = new Date(response.data.time * 1000);
+
   cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = temperature;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  feelsLikeElement.innerHTML = `${feelsLike}°C`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  locationCityElement.innerHTML = `${locationCity}`;
+  locationCountryElement.innerHTML = `${locationCountry}`;
+  longitudeElement.innerHTML = response.data.coordinates.longitude;
+  latitudeElement.innerHTML = response.data.coordinates.latitude;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  } else {
+    minutes;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  } else {
+    hours;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 function searchCity(city) {
   let apiKey = '47ce0ocdabaf4a2e81b031bb9t47a0e0';
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(update);
-  //make API call
-  //update UI
 }
 function search(event) {
   event.preventDefault();
