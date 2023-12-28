bde883f3e61f8a8252814668e4a99deb3fa1f9e8
Emily Ingraham <emilyvingraham@gmail.com>
2023-12-28 08:21:39 -0700

    Added search engine functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert('Hello');
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector('#enter-a-city');
+  let cityElement = document.querySelector('#weather-app-city');
+  cityElement.innerHTML = searchInput.value;
+  //call the API
+  //search for the city
+}
+
+let searchFormElement = document.querySelector('#search-container');
+searchFormElement.addEventListener('submit', search);
