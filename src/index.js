defb2d0fa6c110182206bd16338c2d36dceeae36
Emily Ingraham <emilyvingraham@gmail.com>
2023-12-28 08:41:20 -0700

    Updated search engine to inject city name and temperature from API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function update(response) {
+  let temperatureElement = document.querySelector('#current-temperature');
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector('#weather-app-city');
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+function searchCity(city) {
+  let apiKey = '47ce0ocdabaf4a2e81b031bb9t47a0e0';
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(update);
+  //make API call
+  //update UI
+}
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector('#enter-a-city');
-  let cityElement = document.querySelector('#weather-app-city');
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
   //call the API
   //search for the city
 }
