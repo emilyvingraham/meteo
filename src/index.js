956a4ee052904260f0d8b34f711d2a64e4496310
Emily Ingraham <emilyvingraham@gmail.com>
2023-12-30 14:48:20 -0700

    replaced dummy content with real content using the API

--- src/index.js
+++ src/index.js
@@ -67,10 +67,17 @@ function searchCity(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
-function search(event) {
+function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector('#enter-a-city');
   searchCity(searchInput.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[date.getDay()];
 }
 
 function getForecast(city) {
@@ -85,35 +92,36 @@ function displayForecast(response) {
 
   let forecastHtml = '';
 
-  response.data.daily.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `        <div class="row">
-    <div class="col-2">
-    <div class="weather-forecast-day">Tuesday</div>
-    <div>
-    <img
-    src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png"
-    alt="cloudy"
-    width="42"
-    />
-    </div>
-    <div class="weather-forecast-temperatures">
-    <span class="weather-forecast-temperature-max"> <strong>${Math.round(
-      day.temperature.maximum
-    )}°</strong> </span>
-    <span class="weather-forecast-temperature-min"> ${Math.round(
-      day.temperature.minimum
-    )}° </span>
-    </div>
-    </div>
-    </div>`;
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `        <div class="row">
+      <div class="col-2">
+      <div class="weather-forecast-day">${formatDay(day.time)}</div>
+      <div>
+      <img class="weather-forecast-icon"
+      src="${day.condition.icon_url}"
+      />
+      </div>
+      <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature-max"> <strong>${Math.round(
+        day.temperature.maximum
+      )}°</strong> </span>
+      <span class="weather-forecast-temperature-min"> ${Math.round(
+        day.temperature.minimum
+      )}° </span>
+      </div>
+      </div>
+      </div>`;
+
+    }
   });
   let forecastElement = document.querySelector('#forecast');
   forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector('#search-container');
-searchFormElement.addEventListener('submit', search);
+searchFormElement.addEventListener('submit', handleSearchSubmit);
 
 searchCity('Lisbon');
